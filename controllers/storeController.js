0bb6b4133373b9f203cc612902f4d67de13a6334
Purvi Shah <purvi@streamlending.com.au>
2018-11-09 17:02:49 +1100

    Add store to db

--- controllers/storeController.js
+++ controllers/storeController.js
@@ -1,3 +1,5 @@
+const mongoose = require('mongoose');
+const Store = mongoose.model('Store');
 exports.homePage = (req, res) =>{
     res.render('index')
 }
@@ -6,6 +8,10 @@ exports.addStore = (req, res) => {
     res.render('addStore', { title: 'Add Store'})
 }
 
-exports.createStore = (req, res) => {
-    console.log('response', req.body)
+exports.createStore = async (req, res) => {
+    // either wrap the whole function here in try catch or handle that in error handler which wraps the whole function
+    const store = await(new Store(req.body)).save();
+    req.flash('success', `Successfully Created ${store.name}.Care to leave a review?`);
+    res.redirect(`/store/${store.slug}`);
+
 }
