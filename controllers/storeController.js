001d6e5bfd98f54e1648cafb73962c7beb90d7d7
Purvi Shah <purvi@streamlending.com.au>
2018-11-20 16:57:29 +1100

    save stores

--- controllers/storeController.js
+++ controllers/storeController.js
@@ -21,3 +21,22 @@ exports.getStores = async(req, res)=> {
     const stores = await Store.find();
     res.render('stores', {title: 'Stores', stores})
 }
+
+exports.editStore = async (req, res) => {
+    // 1. find store with the given id
+    const store = await Store.findOne({_id: req.params.id});
+    // 2. confirm they are the owner of the store
+    // TODO
+    // 3. render edit form
+    res.render('addStore', { title: 'Edit Store', store})
+}
+
+exports.updateStore = async (req,res) => {
+    //find and update the store
+    const store = await Store.findOneAndUpdate({_id: req.params.id}, req.body, {
+        new: true, // return the new updated store
+        runValidators: true //force to run the validation of schema which just runs by defualt on create and not on save
+    }).exec();
+    req.flash('success', `Successfully updated <strong>${store.name}</strong><a href="/stores/${store.slug}"> View Store</a>`)
+    res.redirect(`/stores/${store._id}/edit`)
+}
