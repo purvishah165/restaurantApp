c11a21b56725ad97d46e9ad2e3b7e9cfa0b33f8d
Purvi Shah <purvi@streamlending.com.au>
2019-01-09 15:30:12 +1100

    upload and resize images

--- controllers/storeController.js
+++ controllers/storeController.js
@@ -1,5 +1,22 @@
 const mongoose = require('mongoose');
 const Store = mongoose.model('Store');
+const multer = require('multer');
+const jimp = require('jimp');
+const uuid = require('uuid');
+
+const multerOptions = {
+    storage: multer.memoryStorage(),
+    fileFilter(req, file, next){
+        console.log('mime---------------',file.mimetype)
+        const isPhoto = file.mimetype.startsWith('image/');
+        if(isPhoto) {
+            next(null, true);
+        } else {
+            next({message: 'That filetype isn\'t allowed'}, false);
+        }
+
+    }
+}
 exports.homePage = (req, res) =>{
     res.render('index')
 }
@@ -8,7 +25,27 @@ exports.addStore = (req, res) => {
     res.render('addStore', { title: 'Add Store'})
 }
 
+exports.upload = multer(multerOptions).single('photo');
+
+exports.resize = async(req, res, next)=> {
+    //check if there is no file to resize
+    console.log(req.file)
+    if(!req.file) {
+        next(); //skip to next middleware
+        return;
+    }
+    console.log(req.file)
+    const extension = req.file.mimetype.split('/')[1];
+    req.body.photo = `${uuid.v4()}.${extension}`;
+    // now we resize
+    const photo = await jimp.read(req.file.buffer);
+    await photo.resize(150, jimp.AUTO);
+    await photo.write(`./public/uploads/${req.body.photo}`);
+    next();
+}
+
 exports.createStore = async (req, res) => {
+    console.log('hiiiiiiiii')
     // either wrap the whole function here in try catch or handle that in error handler which wraps the whole function
     const store = await(new Store(req.body)).save();
     req.flash('success', `Successfully Created ${store.name}.Care to leave a review?`);
@@ -32,6 +69,8 @@ exports.editStore = async (req, res) => {
 }
 
 exports.updateStore = async (req,res) => {
+    //set location type to Point coz on update it doesnt set
+    req.body.location.type = 'Point';
     //find and update the store
     const store = await Store.findOneAndUpdate({_id: req.params.id}, req.body, {
         new: true, // return the new updated store
---
+++ public/javascripts/modules/autocomplete.js
@@ -0,0 +1,16 @@
+function autocomplete(input, latInput, lngInput) {
+    if(!input) return; //skip autocomplete if there is no address
+    const dropdown = new google.maps.places.Autocomplete(input)
+
+    dropdown.addListener('place_changed', ()=> {
+        const place = dropdown.getPlace();
+        latInput.value = place.geometry.location.lat();
+        lngInput.value = place.geometry.location.lng();
+    });
+    //if someone hits enter on address filed dont submit the form
+    input.on('keydown', (e) => {
+        if (e.keyCode === 13) e.preventDefault();
+    })
+}
+
+export default autocomplete;
