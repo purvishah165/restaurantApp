9aed437c829eaec78edfd1148742e2b2c5e9c7a4
Purvi Shah <purvi@streamlending.com.au>
2019-01-22 17:36:14 +1100

    filter out store related to tag selected and display

--- controllers/storeController.js
+++ controllers/storeController.js
@@ -87,8 +87,12 @@ exports.getStoreBySlug = async (req, res, next) => {
 }
 
 exports.getStoresByTag = async (req, res) => {
-    const tags = await Store.getTagsList();
     const tag = req.params.tag
-    res.render('tags', {tags, title: 'Tags', tag});
+    const tagQuery = tag || {$exists: true};
+    const tagsPromise = Store.getTagsList();
+    const storePromise = Store.find({tags: tagQuery})
+    const [tags, stores] = await Promise.all([tagsPromise, storePromise]);
+
+    res.render('tags', {tags, title: 'Tags', tag, stores});
 
 }
